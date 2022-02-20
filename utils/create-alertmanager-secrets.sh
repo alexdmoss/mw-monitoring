a28cbe5b7b8ae3f5d4377f5f32fa10cf4e5d5c6f
Alex M <alex@mosstech.io>
2022-02-20 18:32:06 +0000

    Adapts utility script for AM secret creation to work without specifying the value at the time

--- utils/create-alertmanager-secrets.sh
+++ utils/create-alertmanager-secrets.sh
@@ -4,10 +4,9 @@ set -euoE pipefail
 if [[ -z ${GCP_PROJECT_ID:-} ]]; then echo "-> [ERROR] GCP_PROJECT_ID not set"; exit 1; fi
 
 if [[ -z ${1:-} ]]; then
-  echo "Secret value not specified - supply it now:"
-  read -r secret_str
+  echo "Secret value not specified - value will not be set"
 else
-  secret_str=${1}
+  echo -n "${1}" | gcloud secrets versions add "ALERTMANAGER_SENDGRID_KEY" --data-file=- --project="${GCP_PROJECT_ID}"
 fi
 
 echo "Creating secret if doesn't exist ..."
@@ -15,5 +14,3 @@ echo "Creating secret if doesn't exist ..."
 gcloud secrets create ALERTMANAGER_SENDGRID_KEY --project="${GCP_PROJECT_ID}" || true
 
 echo "Updating secret version ..."
-
-echo -n "${secret_str}" | gcloud secrets versions add "ALERTMANAGER_SENDGRID_KEY" --data-file=- --project="${GCP_PROJECT_ID}"
