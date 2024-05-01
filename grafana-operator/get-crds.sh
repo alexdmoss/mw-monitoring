595fd89c15d7af7856e998613c0928a010fdf5bd
Alex M <alex@alexos.dev>
2024-05-01 23:55:53 +0100

    Fix: Silliness

--- grafana-operator/get-crds.sh
+++ grafana-operator/get-crds.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -euoE pipefail
 
-curl -Lo ./crds/crd-dashboard.yaml https://github.com/grafana/grafana-operator/blob/master/deploy/helm/grafana-operator/crds/grafana.integreatly.org_grafanadashboards.yaml
-curl -Lo ./crds/crd-datasource.yaml https://github.com/grafana/grafana-operator/blob/master/deploy/helm/grafana-operator/crds/grafana.integreatly.org_grafanadatasources.yaml
-curl -Lo ./crds/crd-folder.yaml https://github.com/grafana/grafana-operator/blob/master/deploy/helm/grafana-operator/crds/grafana.integreatly.org_grafanafolders.yaml
-curl -Lo ./crds/crd-list.yaml https://github.com/grafana/grafana-operator/blob/master/deploy/helm/grafana-operator/crds/grafana.integreatly.org_grafanas.yaml
-curl -Lo ./crds/crd-alertrulegroups.yaml https://github.com/grafana/grafana-operator/blob/master/deploy/helm/grafana-operator/crds/grafana.integreatly.org_grafanaalertrulegroups.yaml
-curl -Lo ./crds/crd-contactpoints.yaml https://github.com/grafana/grafana-operator/blob/master/deploy/helm/grafana-operator/crds/grafana.integreatly.org_grafanacontactpoints.yaml
+curl -Lo ./crds/crd-dashboard.yaml          https://raw.githubusercontent.com/grafana/grafana-operator/master/deploy/helm/grafana-operator/crds/grafana.integreatly.org_grafanadashboards.yaml
+curl -Lo ./crds/crd-datasource.yaml         https://raw.githubusercontent.com/grafana/grafana-operator/master/deploy/helm/grafana-operator/crds/grafana.integreatly.org_grafanadatasources.yaml
+curl -Lo ./crds/crd-folder.yaml             https://raw.githubusercontent.com/grafana/grafana-operator/master/deploy/helm/grafana-operator/crds/grafana.integreatly.org_grafanafolders.yaml
+curl -Lo ./crds/crd-list.yaml               https://raw.githubusercontent.com/grafana/grafana-operator/master/deploy/helm/grafana-operator/crds/grafana.integreatly.org_grafanas.yaml
+curl -Lo ./crds/crd-alertrulegroups.yaml    https://raw.githubusercontent.com/grafana/grafana-operator/master/deploy/helm/grafana-operator/crds/grafana.integreatly.org_grafanaalertrulegroups.yaml
+curl -Lo ./crds/crd-contactpoints.yaml      https://raw.githubusercontent.com/grafana/grafana-operator/master/deploy/helm/grafana-operator/crds/grafana.integreatly.org_grafanacontactpoints.yaml
